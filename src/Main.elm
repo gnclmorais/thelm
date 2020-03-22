809e752e921a3cc02202f96c9e1029affafe7c43
Gonçalo Morais <gnclmorais@gmail.com>
2020-03-22 16:04:26 +0000

    Format document and autofocus

--- src/Main.elm
+++ src/Main.elm
@@ -1,30 +1,32 @@
 module Main exposing (..)
 
 import Browser
-import List.Extra exposing (remove)
-import Html exposing (Html, Attribute, a, button, div, form, input, text, ul, li)
+import Html exposing (Attribute, Html, a, button, div, form, input, li, text, ul)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput, onClick, onSubmit)
-
+import Html.Events exposing (onClick, onInput, onSubmit)
+import List.Extra exposing (remove)
 import Regex
-
 
 
 
 -- MODEL
 
-type alias Reference = String
+
+type alias Reference =
+    String
+
 
 type alias Model =
-  { openReference : Reference
-  , referenceList : List Reference
-  }
+    { openReference : Reference
+    , referenceList : List Reference
+    }
+
 
 init : Model
 init =
-  { openReference = ""
-  , referenceList = []
-  }
+    { openReference = ""
+    , referenceList = []
+    }
 
 
 
@@ -32,92 +34,113 @@ init =
 
 
 main =
-  Browser.sandbox
-    { init = init
-    , update = update
-    , view = view
-    }
+    Browser.sandbox
+        { init = init
+        , update = update
+        , view = view
+        }
 
 
 
 -- UPDATE
 
+
 type Msg
-  = SaveReference Reference
-  | AddReference
-  | RemoveReference Reference
+    = SaveReference Reference
+    | AddReference
+    | RemoveReference Reference
+
 
 update : Msg -> Model -> Model
 update msg model =
-  case msg of
-    SaveReference ref ->
-      { model | openReference = ref }
-    AddReference ->
-      if not(String.isEmpty model.openReference) then
-        { model
-        | openReference = ""
-        , referenceList = model.openReference :: model.referenceList
-        }
-      else
-        model
-    RemoveReference ref ->
-      { openReference = ""
-      , referenceList = remove ref model.referenceList
-      }
+    case msg of
+        SaveReference ref ->
+            { model | openReference = ref }
+
+        AddReference ->
+            if not (String.isEmpty model.openReference) then
+                { model
+                    | openReference = ""
+                    , referenceList = model.openReference :: model.referenceList
+                }
+
+            else
+                model
+
+        RemoveReference ref ->
+            { openReference = ""
+            , referenceList = remove ref model.referenceList
+            }
+
 
 
 -- VIEW
 
+
 view : Model -> Html Msg
 view model =
-  div []
-    [ renderReferenceList model.referenceList
-    , Html.form
-      [ onSubmit AddReference ]
-      [ input
-        [ value model.openReference
-        , onInput SaveReference ] []
-      , button
-        [ type_ "button"
-        , onClick AddReference ] [ text "+" ]
-      ]
-    ]
+    div []
+        [ renderReferenceList model.referenceList
+        , Html.form
+            [ onSubmit AddReference ]
+            [ input
+                [ value model.openReference
+                , autofocus True
+                , onInput SaveReference
+                ]
+                []
+            , button
+                [ type_ "button"
+                , onClick AddReference
+                ]
+                [ text "+" ]
+            ]
+        ]
 
 
 
 -- FUNCTIONS
 
+
 renderReferenceList : List Reference -> Html Msg
 renderReferenceList lst =
     lst
-       |> List.map (\l -> renderReference l)
-       |> ul []
+        |> List.map (\l -> renderReference l)
+        |> ul []
+
 
 renderReference : Reference -> Html Msg
 renderReference ref =
-  li []
-    [ renderRemove ref
-    , renderReferenceText ref ]
+    li []
+        [ renderRemove ref
+        , renderReferenceText ref
+        ]
+
 
 renderReferenceText : String -> Html Msg
 renderReferenceText ref =
-  if validReference ref then
-    a [ href ref ][text ref]
-  else
-    text ref
+    if validReference ref then
+        a [ href ref ] [ text ref ]
+
+    else
+        text ref
+
 
 renderRemove : Reference -> Html Msg
 renderRemove ref =
-  button
-    [ type_ "button"
-    , onClick (RemoveReference ref)]
-    [ text "Remove" ]
+    button
+        [ type_ "button"
+        , onClick (RemoveReference ref)
+        ]
+        [ text "Remove" ]
+
 
 validReference : String -> Bool
 validReference ref =
-  Regex.contains link ref
+    Regex.contains link ref
+
 
 link : Regex.Regex
 link =
-  Maybe.withDefault Regex.never <|
-    Regex.fromString "^(https?://)"
+    Maybe.withDefault Regex.never <|
+        Regex.fromString "^(https?://)"
